f1a6fc3558bb88a0009a410e0aac9fa5339b58e8
Denny29 <43240668+Denny29@users.noreply.github.com>
2021-01-21 16:13:40 -0500

    Did Day 1 Almost did Day 1 and 2 but have a slight problem with the question itself, don't know if right or not.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,8 +2,24 @@ import React, { Component, useState } from "react";
 import "../css/App.css";
 import data from "../sample_data.json";
 
+let questionNum = 0;
+
 function App() {
-  return <div className="app">Trivia!</div>;
+  return (
+    <div className="app">
+      <h1>Trivia!</h1>
+      <Question text="How many states are in the United States?" />
+      <NextQuestion />
+    </div>
+  );
+}
+
+function Question(props) {
+  return <p>{props.text}</p>;
+}
+
+function NextQuestion() {
+  return <button>NextQuestion</button>;
 }
 
 export default App;
